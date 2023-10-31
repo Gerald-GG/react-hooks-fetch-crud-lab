554b89f4a9036f7723fa24acf62e2d8b68fb2609
Gerald Gicharu <gicharugerald@gmail.com>
2023-10-31 08:14:49 +0300

    Completed Assignment

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,33 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [queries, setQueries] = useState([])
+  const[question, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/questions`)
+    .then((res)=> res.json())
+    .then((data)=> {setQueries(data)})
+  })
+
+  function deleteQuiz(id) {
+    const update =question.filter((question) => question.id !==id);
+    setQuestions(update)
+  }
+
+  const Questions =(queries.map((item) => {
+    return <div key={item.id}>
+      <QuestionItem question={item} deleteQuiz={deleteQuiz}
+      />
+    </div>
+  }))
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{Questions}</ul>
     </section>
   );
 }
